5e5ad953ae888bca40957d79d83c062efed40489
Dolvido <lukecello@gmail.com>
2025-03-27 21:07:29 -0400

    more fixes chasing github autodeployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     remotePatterns: [
       {
@@ -54,4 +53,4 @@ const nextConfig = {
   }
 }
 
-export default nextConfig;
+module.exports = nextConfig
