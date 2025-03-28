850c9d5989998853b6c9c56fec673a14a0e7e0a4
Dolvido <lukecello@gmail.com>
2025-03-28 19:29:10 -0400

    changes for build, removed some things preventing static build

--- next.config.js
+++ next.config.js
@@ -24,33 +24,7 @@ const nextConfig = {
       };
     }
 
-    // Add a rule for binary files used by ONNX Runtime
-    config.module.rules.push({
-      test: /\.node$/,
-      use: 'node-loader',
-    });
-
-    // Exclude ONNX Runtime binary files from being processed
-    config.module.rules.push({
-      test: /onnxruntime_binding\.node$/,
-      loader: 'ignore-loader',
-    });
-
-    // Handle @xenova/transformers package
-    config.module.rules.push({
-      test: /@xenova\/transformers/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['next/babel'],
-        },
-      },
-    });
-
     return config;
-  },
-  experimental: {
-    serverComponentsExternalPackages: ['pdf-parse', '@xenova/transformers', 'onnxruntime-node'],
   }
 }
 
